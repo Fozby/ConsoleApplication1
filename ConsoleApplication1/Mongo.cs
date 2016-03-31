c629e8a3f9ac769a4420cd4ef752a9fb6d4a5fcd
ken.teoh <w_m4@hotmail.com>
2016-03-31 20:13:57 +1100

    1

---
+++ ConsoleApplication1/Mongo.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+using MongoDB.Driver;
+
+namespace ConsoleApplication1
+{
+    class Mongo
+    { 
+        private MongoClient theClient;
+        private IMongoDatabase theDataBase;
+        private IMongoCollection<BsonDocument> theCollection;
+
+        public Mongo()
+        {
+            theClient = new MongoClient("mongodb://localhost:27017");
+            theDataBase = theClient.GetDatabase("foo");
+            theCollection = theDataBase.GetCollection<BsonDocument>("bar");
+        }
+
+        public async Task insertRec()
+        {
+            var document = new BsonDocument
+            {
+                { "name", "MongoDB" },
+                { "type", "Database" },
+                { "count", 1 },
+                { "info", new BsonDocument
+                          {
+                              { "x", 203 },
+                              { "y", 102 }
+                          }}
+            };
+
+            Console.WriteLine("About to add");
+            await theCollection.InsertOneAsync(document);
+            Console.WriteLine("Finished adding");
+        }
+
+        public async Task getRec()
+        {
+            var getDoc = await theCollection.Find(new BsonDocument()).FirstOrDefaultAsync();
+            Console.WriteLine(getDoc.ToString());
+        }
+
+        public async Task getCount()
+        {
+            long count = await theCollection.CountAsync(new BsonDocument());
+            Console.WriteLine(count.ToString());
+        }
+    }
+}
---
+++ ConsoleApplication1/Program.cs
@@ -0,0 +1,40 @@
+﻿using MongoDB.Bson;
+using MongoDB.Driver;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApplication1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Mongo mongo = new Mongo();
+
+            while (true)
+            {
+                String input = Console.ReadLine();
+
+                if (input == "get")
+                {
+                    Task.Run(async () => await mongo.getRec());
+                }
+                if (input == "insert")
+                {
+                    Task.Run(async () => await mongo.insertRec());
+                }
+                if (input == "count")
+                {
+                    Task.Run(async () => await mongo.getCount());
+                }
+            }
+        }
+    }
+
+}
