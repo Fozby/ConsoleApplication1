5a18ec1c89313408a5c093991f2e8f9fcad48fcb
firao <fir4g4@gmail.com>
2016-04-10 20:27:53 +1200

    Add hard limit

--- ConsoleApplication1/Riot.cs
+++ ConsoleApplication1/Riot.cs
@@ -3,14 +3,16 @@ using ConsoleApplication1.JsonObjects.MatchObjects;
 using ConsoleApplication1.Objects;
 using RestSharp;
 using System;
+using System.Collections.Concurrent;
 using System.Collections.Generic;
 using System.Linq;
 using System.Net;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace ConsoleApplication1
-{ 
+{
     class Riot
     {
         private const string API_KEY = "9b995c6c-7e5a-4c7a-b905-aab1928af045";
@@ -26,9 +28,8 @@ namespace ConsoleApplication1
         private const string RECENT_GAMES_RESOURCE = "api/lol/" + REGION + "/v1.3/game/by-summoner/{0}/recent?api_key=" + API_KEY;
         private const string MATCH_RESOURCE = "api/lol/" + REGION + "/v2.2/match/{0}?api_key=" + API_KEY;
 
-
         private RestClient myRestClient = new RestClient(BASE_URI);
-        
+
         public List<Game> getRecentGames()
         {
             //default summonerid
@@ -38,33 +39,18 @@ namespace ConsoleApplication1
         public List<Game> getRecentGames(long summonerId)
         {
             string resource = String.Format(RECENT_GAMES_RESOURCE, summonerId);
+            Response_RecentGames response = RiotApiRequest<Response_RecentGames>(resource);
 
-            RestRequest request = new RestRequest(resource, Method.GET);
-            IRestResponse<Response_RecentGames> response = myRestClient.Execute<Response_RecentGames>(request);
-
-            RiotHttpStatusCode statusCode = (RiotHttpStatusCode)response.StatusCode;
-
-            switch (statusCode)
+            List<Game> games = response?.games ?? new List<Game>(); // don't die on null response
+            foreach (Game game in games)
             {
-                case RiotHttpStatusCode.OK: //200
-                {
-                    List<Game> games = response.Data.games;
-                    foreach (Game game in games)
-                    {
-                        game.summonerId = summonerId;
-                    }
-
-                    return games;
-                }
-                default:
-                {
-                    Console.Error.WriteLine($"Response returned status: {statusCode}");
-                    break;
-                }
+                game.summonerId = summonerId;
             }
-            return new List<Game>();
+
+            return games;
+
         }
-        
+
         public List<Game> getRecentGamesForAllPlayers()
         {
             List<Game> games = getRecentGames(SUMMONER_ID_ETARAM);
@@ -103,17 +89,30 @@ namespace ConsoleApplication1
         public MatchDetails getMatch(long gameId)
         {
             string resource = String.Format(MATCH_RESOURCE, gameId);
+            return RiotApiRequest<MatchDetails>(resource);
+        }
 
+        private T RiotApiRequest<T>(string resource) where T : new()
+        {
             RestRequest request = new RestRequest(resource, Method.GET);
-            IRestResponse<MatchDetails> response = myRestClient.Execute<MatchDetails>(request);
+            IRestResponse<T> response = myRestClient.Execute<T>(request);
 
             RiotHttpStatusCode statusCode = (RiotHttpStatusCode)response.StatusCode;
-
             switch (statusCode)
             {
                 case RiotHttpStatusCode.OK: //200
                     {
                         return response.Data;
+                    }
+                case RiotHttpStatusCode.RateLimitExceeded:
+                    {
+                        string retry = response.Headers.Where(a => a.Name.Equals("Retry-After")).Select(b => b.Value.ToString()).FirstOrDefault();
+                        int retrySeconds;
+                        if (!int.TryParse(retry, out retrySeconds)) retrySeconds = 10;
+                        ++retrySeconds;
+                        Console.WriteLine($"You hit the hard limit, pause for {retrySeconds} seconds");
+                        Thread.Sleep(retrySeconds * 1000);
+                        break;
                     }
                 default:
                     {
@@ -121,8 +120,7 @@ namespace ConsoleApplication1
                         break;
                     }
             }
-
-            return null;
+            return default(T);
         }
 
     }
